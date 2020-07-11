b932bddfd7c937d18e9c9a3ca1a6a4c5ec71e129
taruntailor7 <taruntailor7@gmail.com>
2020-07-11 17:08:47 +0530

    h

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomepageComponent, children:[
+                                                    {path:'home',component:HomeComponent},
+                                                    {path:'about',component:AboutComponent},
+                                                    {path:'gallery',component:GalleryComponent},
+                                                    {path:'contacts',component:ContactsComponent}
+  ]},
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
